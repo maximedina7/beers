6f414c95012374c0c260f78a5354eafb515d087b
maximedina7 <maxidomedina@gmail.com>
2025-05-11 19:24:15 -0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; // Cambié la importación agregando la extensión
 import './index.css'; 
 
-// Aseguramos que el React.StrictMode está activado para detectar posibles problemas
+// Crear la raíz de la aplicación y renderizar el componente App
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
